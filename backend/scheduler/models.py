8f446189b6bea442da1032bea83ce57fb5fd63c4
Lucas <lucas@propi.dev>
2023-06-06 18:50:04 -0300

    added user and teacher classes to db

--- backend/scheduler/models.py
+++ backend/scheduler/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.IntegerField("ID")
+    name = models.CharField("Name", max_length=200)
+    email = models.EmailField()
+    password = models.CharField("Password", max_length=200)
+
+    def __str__(self):
+        return self.email
+
+class Teacher(models.Model):
+    register = models.IntegerField("Register")
+
+    def __str__(self):
+        return self.email
+
+# class Admin(models.Model):
+#
+#     def __str__(self):
+#         return self.id
