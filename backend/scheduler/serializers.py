0f398ec41df97e254c088aee93123dcc47b5f8f3
Lucas <propilideno@gmail.com>
2023-06-10 14:09:08 -0300

    added more serializers

--- backend/scheduler/serializers.py
+++ backend/scheduler/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
-from .models import Scheduling
+from .models import Scheduling, Teacher, Admin, Department, Laboratory
 
 class SchedulingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Scheduling
+        fields = '__all__'
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+
+class AdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Admin
+        fields = '__all__'
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+class LaboratorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Laboratory
         fields = '__all__'
