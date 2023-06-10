0f838112e39d65eeb063905b9811a815f84e1cb6
Lucas <propilideno@gmail.com>
2023-06-10 19:37:24 -0300

    running 2 servers on the same time, and handling SIGINT

--- run.sh
+++ run.sh
@@ -1,28 +1,63 @@
 #!/bin/bash
 
-front_port=4040
-back_port=3030
+front_port=3000
+back_port=8000
 
-function back_install_deps(){
-	pip install virtualenv || pip3 install virtualenv
+kill_port(){
+	# Find the process ID (PID) using lsof
+	local PID=$(lsof -t -i :$1)
+
+	# Check if the PID exists
+	if [ -z "$PID" ]; then
+	  echo "No process is running on port 8000."
+	else
+	  # Kill the process using kill command
+	  kill $PID
+	  echo "Process with PID $PID has been killed."
+	fi
 }
 
-function back_run_server(){
+back_install_deps(){
+	# pip install virtualenv || pip3 install virtualenv
+	pip install -r ./backend/requirements.txt || pip3 install -r ./backend/requirements.txt
+}
+
+back_run_server(){
 	# source ./backend/venv/bin/activate #Activate environment
+	kill_port "$back_port"
 	open "http://127.0.0.1:$back_port"
 	python ./backend/manage.py runserver "$back_port" || python3 ./backend/manage.py runserver "$back_port"
 }
 
-function front_install_deps(){
+front_install_deps(){
 	npm install
 }
 
-function front_run_server(){
-	npm start
+front_run_server(){
+	kill_port "$front_port"
+	cd ./frontend
+	npm start #Automatically open browser when started
+	cd ..
 }
 
-function main(){
-	front_install_deps
+kill_process(){
+	kill_port "$front_port"
+	kill_port "$back_port"
 }
 
-back_run_server
+main(){
+	# Set up a signal handler for SIGINT (Ctrl+C)
+	trap 'kill_process' SIGINT
+
+	back_install_deps
+ 	# front_install_deps
+
+	back_run_server &
+	front_run_server &
+	
+	# Wait for SIGINT to trigger kill process
+	sleep infinity
+}
+
+main
+
