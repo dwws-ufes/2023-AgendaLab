738295259bedb4c214479d1cbeda3c93a3d2b25e
Lucas <propilideno@gmail.com>
2023-06-12 23:51:40 -0300

    added put,edit and get method to .sh

--- post.sh
+++ post.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+backendUrl="http://localhost:8080"
+
 createUser(){
-	curl -X POST -H "Content-Type: application/json" -d '{"name":"Fulano senha fraca","email":"fulano@email.com","password":"senhafraca321"}' http://localhost:8080/request/user/
+	curl -X POST -H "Content-Type: application/json" -d '{"name":"Fulano senha fraca","email":"fulano@email.com","password":"senhafraca321"}' $backendUrl/request/user/
+}
+
+editUser(){
+	curl -X PUT -H "Content-Type: application/json" -d '{"name":"Fulano senha fraca","password":"senhafraca321"}' $backendUrl/request/user/$1
+}
+
+deleteUser(){
+	curl -X DELETE $backendUrl/request/user/$1
+}
+
+getUser(){
+	curl -X GET $backendUrl/request/user/$1
 }
 
 main(){
