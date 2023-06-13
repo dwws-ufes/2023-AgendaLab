d1e5f2a04c69a1724a8d7fff5283ae1e5778481a
Lucas <propilideno@gmail.com>
2023-06-13 00:06:25 -0300

    improved request

--- request.sh
+++ request.sh
@@ -4,9 +4,9 @@ backendUrl="http://localhost:8080"
 
 ######## NON POST METHODS #########
 
-editUser(){
-	curl -X PUT -H "Content-Type: application/json" -d '{"name":"Fulano senha fraca","password":"senhafraca321"}' $backendUrl/request/user/$1
-}
+# editUser(){
+# 	curl -X PUT -H "Content-Type: application/json" -d '{"name":"Fulano senha fraca","password":"senhafraca321"}' $backendUrl/request/user/$1
+# }
 
 deleteUser(){
 	curl -X DELETE $backendUrl/request/user/$1
@@ -19,11 +19,13 @@ getUser(){
 
 ######## POST METHODS #########
 
-createUser(){
-	curl -X POST -H "Content-Type: application/json" -d '{"name":"Fulano senha fraca","email":"fulano@email.com","password":"senhafraca321"}' $backendUrl/request/user/
+createUser() {
+  curl -X POST -H "Content-Type: application/json" -d '{"name":"'"$1"'","email":"'"$2"'","password":"'"$3"'"}' "$backendUrl/request/user/"
 }
 
 
 main(){
-	createUser()
+	createUser "Fulano Senha Fraca" "fulano@email.com" "senhafraca321"
 }
+
+main
