7c2df13a98b61b85607241ae6b72d034132241dc
joaobiancardi <joao.biancardi@edu.ufes.br>
2023-06-18 23:08:17 -0300

    Adicionar modal para cadastro de entidades

--- frontend/src/components/tabView.tsx
+++ frontend/src/components/tabView.tsx
@@ -1,7 +1,20 @@
 import { TabPanel, TabView } from "primereact/tabview";
 import DataTableComponent from "./dataTable";
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import { useState } from "react";
+import { InputText } from "primereact/inputtext";
 
 function TabViewComponent(props: any) {
+
+  const [visible, setVisible] = useState(false);
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleTabChange = (event: any) => {
+    setActiveIndex(event.index);
+  };
+
   const teacherTableHeaders =
     props.teachersTable?.length > 0 ? Object.keys(props.teachersTable[0]) : [];
 
@@ -16,9 +29,16 @@ function TabViewComponent(props: any) {
   return (
     <div>
       <div className="d-flex justify-content-end p-2">
-        <button className="btn btn-outline-light">Adicionar</button>
+        <Button label="Show" icon="pi pi-external-link" onClick={() => setVisible(true)} />
+        <Dialog header="Header" visible={visible} style={{ width: '50vw' }} onHide={() => setVisible(false)}>
+        <span className="p-float-label">
+            <InputText id="username" />
+            <label htmlFor="username">Username</label>
+        </span>
+        </Dialog>
       </div>
-      <TabView>
+
+      <TabView activeIndex={activeIndex} onTabChange={handleTabChange}>
         <TabPanel header="Professores">
           <DataTableComponent
             products={props.teachersTable}
