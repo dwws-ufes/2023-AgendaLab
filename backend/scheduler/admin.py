762381f4c9f3a6fb831bb827ed2f618690aeea01
lucas <lucas@propi.dev>
2023-06-19 09:51:19 -0300

    hiding user creation

--- backend/scheduler/admin.py
+++ backend/scheduler/admin.py
@@ -20,18 +20,7 @@ class TeacherAdmin(admin.ModelAdmin):
             obj.set_password(password)  # Faz o hash da senha
         super().save_model(request, obj, form, change)
 
-class CustomUserAdmin(admin.ModelAdmin):
-    exclude = ['last_login']
-    def save_model(self, request, obj, form, change):
-        # Verifica se a senha foi alterada
-        if 'password' in form.changed_data:
-            password = form.cleaned_data['password']
-            obj.set_password(password)  # Faz o hash da senha
-        super().save_model(request, obj, form, change)
-
-admin.site.register(User,CustomUserAdmin)
 admin.site.register(Teacher, TeacherAdmin)
-# admin.site.register(Admin)
 admin.site.register(Department)
 admin.site.register(Laboratory, LaboratoryAdmin)
 admin.site.register(Scheduling, SchedulingAdmin)
