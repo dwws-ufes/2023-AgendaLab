051f81313211b43767bf6577edf50489211fe840
lucas <lucas@propi.dev>
2023-07-20 22:45:23 -0300

    fixing requirement script

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,4 @@
-pip install -r requirements.txt
+pip install -r backend/requirements.txt
 pip install -e backend/sparql/cucopy/.
 pip install -e backend/sparql/techapi/.
 pip install -e backend/sparql/django-rdf-io/.
