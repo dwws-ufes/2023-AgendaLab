ae2eeb134cae1a1c1a2fc36b5309c4d55f1a3073
lucas <lucas@propi.dev>
2023-07-20 23:08:46 -0300

    cucopy added

---
+++ backend/sparql/cucopy/cucopy/__init__.py
@@ -0,0 +1,9 @@
+"""
+**CuCoPy** is a Python library for adjusting euro values for inflation.
+"""
+
+__version__ = "1.0"
+__author__ = 'Julian Schönau, Patrick Kuckertz, Jann Weinand, Leander Kotzur, Detlef Stolten'
+
+import os, pathlib
+from .currency import Currency
